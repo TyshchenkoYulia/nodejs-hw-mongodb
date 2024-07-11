2cddf2b20041e1275715e1747369ed133cef3e9c
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-07-11 16:01:06 +0300

    finish

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,31 +1,50 @@
 import Joi from 'joi';
-import {
-  phoneNumberValidation,
-  typeList,
-} from '../constants/contacts-constants.js';
+// import {
+//   phoneNumberValidation,
+//   typeList,
+// } from '../constants/contacts-constants.js';
 
 export const contactAddSchema = Joi.object({
   name: Joi.string().required().min(3).max(20),
-  phoneNumber: Joi.string()
-    .pattern(phoneNumberValidation)
-    .required()
-    .min(3)
-    .max(20),
+  phoneNumber: Joi.string().required().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-    .valid(...typeList)
-    .min(3)
-    .max(20),
+  contactType: Joi.string().valid('work', 'home', 'personal').min(3).max(20),
 });
 
 export const contactUpdateSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().pattern(phoneNumberValidation).min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-    .valid(...typeList)
-    .min(3)
-    .max(20),
+  contactType: Joi.string().valid('work', 'home', 'personal').min(3).max(20),
 });
+
+// export const contactAddSchema = Joi.object({
+//   name: Joi.string().required().min(3).max(20),
+//   phoneNumber: Joi.string()
+//     // .pattern(phoneNumberValidation)
+//     .required()
+//     .min(3)
+//     .max(20),
+//   email: Joi.string().email().min(3).max(20),
+//   isFavourite: Joi.boolean(),
+//   contactType: Joi.string()
+//     .valid(...typeList)
+//     .min(3)
+//     .max(20),
+// });
+
+// export const contactUpdateSchema = Joi.object({
+//   name: Joi.string().min(3).max(20),
+//   phoneNumber: Joi.string()
+//     // pattern(phoneNumberValidation).
+//     .min(3)
+//     .max(20),
+//   email: Joi.string().email().min(3).max(20),
+//   isFavourite: Joi.boolean(),
+//   contactType: Joi.string()
+//     .valid(...typeList)
+//     .min(3)
+//     .max(20),
+// });
