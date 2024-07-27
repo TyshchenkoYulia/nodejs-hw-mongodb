0ec67aff505507289ae3acb1201117474a8b6814
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-07-27 14:00:02 +0300

    finish

--- src/services/auth.js
+++ src/services/auth.js
@@ -8,10 +8,10 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { Session } from '../db/models/session.js';
 import { createSession } from './session.js';
-import {
-  getFullNameFromGoogleTokenPayload,
-  validateCode,
-} from '../utils/googleOAuth2.js';
+// import {
+//   getFullNameFromGoogleTokenPayload,
+//   validateCode,
+// } from '../utils/googleOAuth2.js';
 
 export const findUser = (filter) => User.findOne(filter);
 
@@ -90,28 +90,4 @@ export const resetPassword = async (payload) => {
   const encryptedPassword = await bcrypt.hash(payload.password, 10);
 
   await User.updateOne({ _id: user._id }, { password: encryptedPassword });
-};
-
-export const loginOrSignupWithGoogle = async (code) => {
-  const loginTicket = await validateCode(code);
-  const payload = loginTicket.getPayload();
-  if (!payload) throw createHttpError(401);
-
-  let user = await User.findOne({ email: payload.email });
-  if (!user) {
-    const password = await bcrypt.hash(randomBytes(10), 10);
-    user = await User.create({
-      email: payload.email,
-      name: getFullNameFromGoogleTokenPayload(payload),
-      password,
-      role: 'parent',
-    });
-  }
-
-  const newSession = createSession();
-
-  return await Session.create({
-    userId: user._id,
-    ...newSession,
-  });
 };
